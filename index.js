4c7350daa2b5ec8df3696f11eb6d07f66bcadb86
Liza Parr <lizajparr@gmail.com>
2023-03-01 15:16:37 -0500

    starting code for HW

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+// DEPENDENCIES
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { title } = require('process');
 
+// DATA
 // TODO: Create an array of questions for user input
-const questions = [];
+// added spread operator to .prompt
+const questions = [
+    {
+        type: 'input',
+        message: "What is the title of your README?",
+        name: "title"
+    }
+];
 
+// FUNCTIONS
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+init() {}
 
-// Function call to initialize app
-init();
+// USER INTERACTION
+inquirer
+  .prompt([...questions]
+    // pass your questions in here
+  )
+  .then((answers) => {
+    // use user feedback for...whatever!
+  })
